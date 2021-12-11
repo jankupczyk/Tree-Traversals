c70eae2aa27396cf917c23c260356237f523a1d3
Jan Kupczyk <73750630+jankupczyk@users.noreply.github.com>
2021-12-11 01:03:33 +0100

    Add files via upload

---
+++ BST.py
@@ -0,0 +1,65 @@
+class Node:
+	def __init__(self, key):
+		self.left = None
+		self.right = None
+		self.val = key
+
+
+def insert(root, key):
+	if root is None:
+		return Node(key)
+	else:
+		if root.val == key:
+			return root
+		elif root.val < key:
+			root.right = insert(root.right, key)
+		else:
+			root.left = insert(root.left, key)
+	return root
+
+
+def inorder(root):
+	if root:
+		inorder(root.left)
+		print(root.val)
+		inorder(root.right)
+
+def postorder(root):
+	if root:
+		postorder(root.left)
+		postorder(root.right)
+		print(root.val)
+
+def preorder(root):
+	if root:
+		print(root.val)
+		preorder(root.left)
+		preorder(root.right)
+
+
+# Tree traversal ROOT
+# i.e {30, 43, 13, 8, 50, 40, 20, 19, 22}
+x = Node(30) # Provide NODE - ROOT counts as #1
+x = insert(x, 43) #2
+x = insert(x, 13) #3
+x = insert(x, 8)  #4
+x = insert(x, 50) #5
+x = insert(x, 40) #6
+x = insert(x, 20) #7
+x = insert(x, 19) #8
+x = insert(x, 22) #9
+#			      #etc...
+
+print('(a) Inorder (Left, Root, Right)')
+inorder(x)
+print('\n(c) Post-order (Left, Right, Root)')
+postorder(x)
+print('\n(b) Pre-order (Root, Left, Right)')
+preorder(x)
+
+
+# BOILERPLATE
+if __name__ == "__main__":
+	BSTmain = Node(0)
+	print(f'\nAUTHOR: Jan Kupczyk')
+	print(f'https://github.com/jankupczyk\n')
